fe181b4e6f97c83e98504d97367bf746d969e74a
ashiqur-r-biplop <biplops22@gmail.com>
2024-02-27 13:21:49 +0600

    added input and set the console in server site

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
+const qrCode = require("qrcode");
 
 const app = express();
 const port = process.env.port || 3000;
@@ -13,6 +14,13 @@ app.set("views", path.join(__dirname, "view"));
 
 app.get("/", (req, res, next) => {
   res.render("index");
+});
+
+app.post("/scan", (req, res, next) => {
+    const startWithLetters = req.body.startWithLetters
+    const SerialNumberStart = req.body.SerialNumberStart
+    const SerialNumberEnd = req.body.SerialNumberEnd
+    console.log(startWithLetters, SerialNumberStart, SerialNumberEnd);
 });
 
 app.listen(port, console.log(`Listening on port ${port}`));

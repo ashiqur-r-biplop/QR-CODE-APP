fc9a523a6c0d31ed297cff6414555a1b8297222c
ashiqur-r-biplop <biplops22@gmail.com>
2024-02-27 12:53:26 +0600

    added ejs file and connect this

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+
 const app = express();
 const port = process.env.port || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
 
-app.get("/", (re))
-
-
-
-
-
-
-
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
 
 app.listen(port, console.log(`Listening on port ${port}`));
